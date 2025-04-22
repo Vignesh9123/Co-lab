f8bde0050de7bce7a57ea70bb9d5102b051e081f
Vignesh D <vignesh.d9123@gmail.com>
2025-04-22 18:41:44 +0530

    Add sidebar

--- apps/web/src/app/(protected)/dashboard/page.tsx
+++ apps/web/src/app/(protected)/dashboard/page.tsx
@@ -1,8 +1,49 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect , useState} from 'react'
 import {useAuthStore} from '@/zustand/auth'
 import { useRouter } from 'next/navigation';
+import { Sidebar ,SidebarLink, SidebarBody} from '@/components/ui/sidebar';
+import { Home, Video, PencilRuler, Youtube, Headphones } from 'lucide-react'
+import {motion} from 'motion/react'
 function Dashboard() {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "#",
+      icon: (
+        <Home className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Video",
+      href: "#",
+      icon: (
+        <Video className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Draw",
+      href: "#",
+      icon: (
+        <PencilRuler className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Watch",
+      href: "#",
+      icon: (
+        <Youtube className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Listen",
+      href: "#",
+      icon: (
+        <Headphones className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
   const {user, loading} = useAuthStore();
   const router = useRouter();
   useEffect(() => {
@@ -11,10 +52,40 @@ function Dashboard() {
     }
   }, [user, router, loading])
   return (
-    <div>
-      Welcome {user?.name}
-    </div>
+    
+      <div className='overflow-y-auto bg-background flex-1' >
+
+      
+      
+      </div>
   )
 }
 
 export default Dashboard
+export const Logo = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium whitespace-pre text-black dark:text-white"
+      >
+        Co-Lab
+      </motion.span>
+    </a>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+    </a>
+  );
+};
---
+++ apps/web/src/components/SidebarComponent.tsx
@@ -0,0 +1,109 @@
+'use client'
+import React, { useState } from 'react'
+import { Sidebar ,SidebarLink, SidebarBody} from '@/components/ui/sidebar';
+import { Home, Video, PencilRuler, Youtube, Headphones } from 'lucide-react'
+import {motion} from 'motion/react'
+import {useAuthStore} from '@/zustand/auth'
+function SidebarComponent() {
+    const links = [
+        {
+          label: "Dashboard",
+          href: "#",
+          icon: (
+            <Home className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+          ),
+        },
+        {
+          label: "Video",
+          href: "#",
+          icon: (
+            <Video className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+          ),
+        },
+        {
+          label: "Draw",
+          href: "#",
+          icon: (
+            <PencilRuler className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+          ),
+        },
+        {
+          label: "Watch",
+          href: "#",
+          icon: (
+            <Youtube className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+          ),
+        },
+        {
+          label: "Listen",
+          href: "#",
+          icon: (
+            <Headphones className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+          ),
+        },
+      ];
+      const [open, setOpen] = useState(false);
+      const {user, loading} = useAuthStore();
+  return (
+    
+      <Sidebar open={open} setOpen={setOpen} >
+              <SidebarBody className="justify-between gap-10">
+                <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+                  {open ? <Logo /> : <LogoIcon />}
+                  <div className="mt-8 flex flex-col gap-2">
+                    {links.map((link, idx) => (
+                      <SidebarLink key={idx} link={link} />
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <SidebarLink
+                    link={{
+                      label: user?.name || "",
+                      href: "#",
+                      icon: (
+                        <img
+                          src={user?.profilePicture}
+                          className="h-7 w-7 shrink-0 rounded-full"
+                          width={50}
+                          height={50}
+                          alt="Avatar"
+                          referrerPolicy='no-referrer'
+                        />
+                      ),
+                    }}
+                  />
+                </div>
+              </SidebarBody>
+            </Sidebar>
+  )
+}
+
+export default SidebarComponent
+export const Logo = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium whitespace-pre text-black dark:text-white"
+      >
+        Co-Lab
+      </motion.span>
+    </a>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+    </a>
+  );
+};
