a0335a3eab8208d7f2fd19559b19712f354f50bb
Vignesh D <vignesh.d9123@gmail.com>
2025-05-07 23:58:13 +0530

    Refactor authStore

--- apps/web/src/zustand/auth.ts
+++ apps/web/src/zustand/auth.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import {User, AuthState} from '@repo/types'
+import { AuthState} from '@repo/types'
 import axiosInstance from "@/config/axiosConfig";
 
 
@@ -12,18 +12,18 @@ export const useAuthStore = create<AuthState>()((set) => (
     isLoggedIn: false,
     setUser: (u) => set(() => ({user:u, isLoggedIn: true })),
     login: async({idToken})=>{
-            set(() => ({loading: true}));
+            set({loading: true});
             const response = await axiosInstance.post('/user/google-auth', { idToken });
             const {user, accessToken} = response.data;
             localStorage.setItem('token', accessToken);
-            set(() => ({user: user, token: accessToken, loading: false, isLoggedIn: true}));      
+            set({user, token: accessToken, loading: false, isLoggedIn: true});      
         
     },
     logout: async() => {
-        set(() => ({loading: true}));
-        const response = await axiosInstance.get('/user/logout');
+        set({loading: true});
+        await axiosInstance.get('/user/logout');
         localStorage.removeItem('token');
-        set(() => ({user: null, token: null, loading: false, isLoggedIn: false}));
+        set({user: null, token: null, loading: false, isLoggedIn: false});
     },
     setLoading: (loading) => set(() => ({loading})),
     setIsLoggedIn: (isLoggedIn) => set(() => ({isLoggedIn})),
