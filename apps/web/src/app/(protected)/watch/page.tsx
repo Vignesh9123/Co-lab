616ad2c9f048e6325a0c29e2997f68f8c434365f
Vignesh D <vignesh.d9123@gmail.com>
2025-05-09 22:25:22 +0530

    feat: update watch page to include persisted streams

--- apps/web/src/app/(protected)/watch/page.tsx
+++ apps/web/src/app/(protected)/watch/page.tsx
@@ -1,10 +1,27 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef} from 'react'
+import {useVideoStore} from '@/zustand/video'
 function Watch() {
-  
+  const myVideo = useRef<HTMLVideoElement>(null)
+  const remoteVideo = useRef<HTMLVideoElement>(null)
+  const {myStream, setMyStream, peerConnection, remoteStream} = useVideoStore()
+
+  useEffect(() => {
+    if(!myStream || !peerConnection || !remoteStream) return
+    myVideo.current!.srcObject = myStream
+    remoteVideo.current!.srcObject = remoteStream
+
+    peerConnection.ontrack = (e) => {
+      remoteVideo.current!.srcObject = e.streams[0]
+    }
+
+
+  }, [myStream, setMyStream, peerConnection])
   return (
     <div>
-      Watch
+      <video className='border border-red-600' ref={myVideo} autoPlay muted />
+      <video className='border border-green-600' ref={remoteVideo} autoPlay/>
+      
     </div>
   )
 }
