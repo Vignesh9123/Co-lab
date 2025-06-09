96a3e9d5fd75f849520120de39c46711c1dd602f
Vignesh D <vignesh.d9123@gmail.com>
2025-06-09 09:15:48 +0530

    add rectangle stroke

--- apps/web/src/app/(protected)/draw/page.tsx
+++ apps/web/src/app/(protected)/draw/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client'
+import { div } from 'motion/react-client';
+import React, { useEffect } from 'react'
 
 function Draw() {
+  useEffect(() => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas?.getContext("2d");
+if(!ctx) return
+window.addEventListener('resize', resizeCanvas, false);
+        
+function resizeCanvas() {
+  canvas.width = window.innerWidth - 50;
+  canvas.height = window.innerHeight;
+    
+}
+
+resizeCanvas();
+ctx.strokeStyle = "red";
+    let initialX;
+    let initialY;
+    let clicked = false;
+    window.addEventListener("mousedown", (e) => {
+      clicked = true;
+      initialX = e.clientX;
+      initialY = e.clientY;
+    })
+    window.addEventListener("mousemove", (e) => {
+      if(clicked) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeRect(initialX, initialY, e.clientX - initialX, e.clientY - initialY);
+      }
+    })
+    window.addEventListener("mouseup", () => {
+      clicked = false;
+    })
+  }, [])
   return (
-    <div>
-      Draw
+    <div className='w-full h-screen'>
+
+    <canvas className='block' id="canvas">
+
+    </canvas>
     </div>
   )
 }
